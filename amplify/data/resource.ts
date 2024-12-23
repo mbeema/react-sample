cdf7aa21bd4d9d975b4f3dcb0904101dfde95d57
Madhukar Beema <mbeema@saavys.com>
2024-12-22 22:08:46 -0500

    Added AI

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -13,6 +13,31 @@ const schema = a.schema({
     })
     //.authorization((allow) => [allow.publicApiKey()]),
     .authorization(allow => [allow.owner()]),
+  // This will add a new conversation route to your Amplify Data backend.
+  chat: a
+    .conversation({
+      aiModel: a.ai.model("Claude 3.5 Haiku"),
+      systemPrompt: "You are a helpful assistant",
+    })
+    .authorization((allow) => allow.owner()),
+
+  // This adds a new generation route to your Amplify Data backend.
+  generateRecipe: a
+    .generation({
+      aiModel: a.ai.model("Claude 3.5 Haiku"),
+      systemPrompt: "You are a helpful assistant that generates recipes.",
+    })
+    .arguments({
+      description: a.string(),
+    })
+    .returns(
+      a.customType({
+        name: a.string(),
+        ingredients: a.string().array(),
+        instructions: a.string(),
+      })
+    )
+    .authorization((allow) => allow.authenticated()),
 });
 
 export type Schema = ClientSchema<typeof schema>;
