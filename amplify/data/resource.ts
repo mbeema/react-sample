159fcdedb714cdfaa139ff6d0fd46e25df1ea025
Madhukar Beema <mbeema@saavys.com>
2024-12-22 23:08:58 -0500

    Added AI

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,14 +3,13 @@ import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 const schema = a.schema({
   // This will add a new conversation route to your Amplify Data backend.
   chat: a.conversation({
-    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    aiModel: a.ai.model('Claude 3 Haiku'),
     systemPrompt: 'You are a helpful assistant',
-  })
-  .authorization((allow) => allow.owner()),
+  }),
 
   // This adds a new generation route to your Amplify Data backend.
   generateRecipe: a.generation({
-    aiModel: a.ai.model('Claude 3.5 Haiku'),
+    aiModel: a.ai.model('Claude 3 Haiku'),
     systemPrompt: 'You are a helpful assistant that generates recipes',
   })
   .arguments({
